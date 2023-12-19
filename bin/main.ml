e76d3e6d52f28c8a4566671336fb544cef55a4fc
Frank O'Brien <fobrien@roblox.com>
2023-12-18 21:20:31 -0800

    a

---
+++ bin/main.ml
@@ -0,0 +1,38 @@
+open Ppx_yojson_conv_lib.Yojson_conv.Primitives
+open Lwt.Syntax
+module UMap = Map.Make (String)
+
+let user_map = ref UMap.empty
+
+type message_object = { assetid : string } [@@deriving yojson]
+
+let () =
+  Dream.run
+  @@ Dream.logger
+  @@ Dream.router
+       [ Dream.get "/asset/:userid" (fun req ->
+           let userid = Dream.param req "userid" in
+           match UMap.find_opt userid !user_map with
+           | Some assetid -> Dream.json (Format.sprintf "{\"value\": %s}" assetid)
+           | None ->
+             Dream.json (Format.sprintf "{\"error\":\"no data for user %s\"}" userid))
+       ; Dream.post "/asset/:userid" (fun req ->
+           let* body = Dream.body req in
+           let success =
+             try
+               let msg = body |> Yojson.Safe.from_string |> message_object_of_yojson in
+               let userid = Dream.param req "userid"
+               and assetid = msg.assetid in
+               user_map := UMap.add userid assetid !user_map;
+               true
+             with
+             | _ -> false
+           in
+           match success with
+           | true -> Dream.json "{}"
+           | false -> Dream.json "{\"error\":\"error parsing input\"}")
+       ; Dream.post "/asset:clear" (fun _ ->
+           user_map := UMap.empty;
+           Dream.json "{}")
+       ]
+;;
---
+++ lib/server/server.ml
@@ -0,0 +1,3 @@
+module Server = struct end
+
+let%test "part2 test" = Alcotest.(check int) "sum" 2 2
